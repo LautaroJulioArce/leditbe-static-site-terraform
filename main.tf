2abbd3ffd47965d26d45da21e48c5b3f2f987e46
Lautaro Arce <lautaroarce89@gmail.com>
2026-07-18 16:24:10 -0300

    Agregar OAC y tags al bucket S3

--- main.tf
+++ main.tf
@@ -4,12 +4,23 @@ provider "aws" {
 
 resource "aws_s3_bucket" "website" {
   bucket = "landing-page-cuadros-led-lautaro"
+
+  tags = {
+    Name        = "Landing Page LED IT BE"
+    Environment = "dev"
+    Project     = "leditbe-terraform"
+    ManagedBy   = "Terraform"
+    Owner       = "Lautaro"
+    Purpose     = "Static website"
+  }
+
 }
 
 
-#esto hace que el bucket sea privado y no pueda ser accedido por nadie. 
-#ni siquiera por el dueño del bucket. Esto es importante para evitar que alguien pueda acceder a los archivos del bucket sin autorización.
-#acls = Access Control List, que es una lista de control de acceso que define quién puede acceder a un recurso y qué acciones pueden realizar sobre él.
+
+# Mantiene el bucket privado y bloquea el acceso público no autorizado.
+# Más adelante se creará un OAC (Origin Access Control) para permitir que CloudFront acceda al bucket de forma segura.
+# ACL significa Access Control List.
 
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket = aws_s3_bucket.website.id
@@ -18,4 +29,17 @@ resource "aws_s3_bucket_public_access_block" "website" {
   ignore_public_acls      = true
   block_public_policy     = true
   restrict_public_buckets = true
+}
+
+
+
+# Crea un OAC (Origin Access Control) para que CloudFront pueda acceder de forma segura al bucket S3 privado.
+# Las solicitudes de CloudFront se firman utilizando AWS Signature Version 4 (SigV4).
+
+resource "aws_cloudfront_origin_access_control" "website" {
+  name                              = "landing-page-cuadros-led-lautaro"
+  description                       = "Acceso seguro de CloudFront al bucket S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
