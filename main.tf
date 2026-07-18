41d3cfbca01149553ea132a406242a9cd7f05300
Lautaro Arce <lautaroarce89@gmail.com>
2026-07-18 15:58:46 -0300

    Bloqueo de acceso por bucket

--- main.tf
+++ main.tf
@@ -5,3 +5,17 @@ provider "aws" {
 resource "aws_s3_bucket" "website" {
   bucket = "landing-page-cuadros-led-lautaro"
 }
+
+
+#esto hace que el bucket sea privado y no pueda ser accedido por nadie. 
+#ni siquiera por el dueño del bucket. Esto es importante para evitar que alguien pueda acceder a los archivos del bucket sin autorización.
+#acls = Access Control List, que es una lista de control de acceso que define quién puede acceder a un recurso y qué acciones pueden realizar sobre él.
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
